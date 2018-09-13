23f3a5aefb2d7ab4e65eb46d1485d6e9cf9295eb
Adam Kulas <adam.kulas1@gmail.com>
2018-09-13 19:42:56 -0400

    Consolidated files and changed main

--- a1ece650.py
+++ a1ece650.py
@@ -1,15 +1,50 @@
-'''
-Some notes:
-Look into cmd module https://stackoverflow.com/questions/17352630/creating-a-terminal-program-with-python
-https://code.activestate.com/recipes/280500-console-built-with-cmd-object/
-'''
-
 import sys
 import shlex
+import cmd
 import argparse
 import re
+print sys.executable
+class Cameraprog(cmd.Cmd):
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.intro = 'Welcome to the camera optimizer program. Type help or ? to list commands'
 
-def parse(line):
+    def do_a(self, line):
+        print 'doing command /"a/"'
+    
+    def help_a(self):
+        print 'this is help string for \"a"'
+
+    def do_r(self, line):
+        print 'doing command /"r/"'
+
+    def do_c(self, line):
+        print 'doing command /"c/"'
+
+    def do_g(self, line):
+        print 'doing command /"g/"'
+
+    def precmd(self, line):
+        return line
+
+    def postcmd(self, stop, line):
+        #If line empty, exit program
+        if not line:
+            stop = True
+        return stop
+
+    def postloop(self):
+        #Cleanup and gracefully exit
+        sys.exit(0)
+
+    def default(self, line):
+        print 'Error: The command you entered was not found'
+        print line
+
+    def emptyline(self):
+        pass
+        
+def parse(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('command', choices=['a','c','r','g'])
     parser.add_argument('street_name')
@@ -31,29 +66,13 @@ def check_coordinate_input(coords):
         if not(coord.startswith('(') and coord.endswith(')')):
             print 'Error: coordinate \"%s\" does not start with \'(\' and end with \')\'' % coord
 
+def main(args):
+    program = Cameraprog()
+    program.prompt = '=-> ' 
+    program.use_rawinput = False
+    program.cmdloop()
 
-    
-
-    #check for starting and ending quotes
-
-
-def main():
-    ### YOUR MAIN CODE GOES HERE
-
-    ### sample code to read from stdin.
-    ### make sure to remove all spurious print statements as required
-    ### by the assignment
-    while True:
-        line = sys.stdin.readline()
-        if line == '\n':
-            break
-        print 'read a line:', line
-        command, street_name, coords = parse(line)
-
-
-    print 'Finished reading input'
-    # return exit code 0 on successful termination
-    sys.exit(0)
+    return 0
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main(sys.argv))
