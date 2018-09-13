778a6c4d735fb466b3ed0ff1bbba4715c446a876
Adam Kulas <adam.kulas1@gmail.com>
2018-09-13 18:31:01 -0400

    Added cmd class

--- a1ece650.py
+++ a1ece650.py
@@ -1,6 +1,41 @@
-import sys
+'''
+Some notes:
+Look into cmd module https://stackoverflow.com/questions/17352630/creating-a-terminal-program-with-python
+https://code.activestate.com/recipes/280500-console-built-with-cmd-object/
+'''
 
-# YOUR CODE GOES HERE
+import sys
+import shlex
+import argparse
+import re
+
+def parse(line):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('command', choices=['a','c','r','g'])
+    parser.add_argument('street_name')
+    parser.add_argument('coords', nargs='+')
+    try:
+        args = parser.parse_args(shlex.split(line))
+    except SystemExit:
+        print 'FAILURE ABORT'
+        return False
+    check_coordinate_input(args.coords)
+    print args.coords
+    return args.command, args.street_name, args.coords
+
+def check_coordinate_input(coords):
+    if len(coords)==0:
+        print "Error: No coordinates were entered"
+        return False
+    for coord in coords:
+        if not(coord.startswith('(') and coord.endswith(')')):
+            print 'Error: coordinate \"%s\" does not start with \'(\' and end with \')\'' % coord
+
+
+    
+
+    #check for starting and ending quotes
+
 
 def main():
     ### YOUR MAIN CODE GOES HERE
@@ -13,6 +48,8 @@ def main():
         if line == '\n':
             break
         print 'read a line:', line
+        command, street_name, coords = parse(line)
+
 
     print 'Finished reading input'
     # return exit code 0 on successful termination
---
+++ a1ece650cmd.py
@@ -0,0 +1,81 @@
+'''
+Some notes:
+Look into cmd module https://stackoverflow.com/questions/17352630/creating-a-terminal-program-with-python
+https://code.activestate.com/recipes/280500-console-built-with-cmd-object/
+'''
+
+import sys
+import shlex
+import cmd
+import argparse
+import re
+print sys.executable
+
+class Cameraprog(cmd.Cmd):
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.intro = 'Welcome to the camera optimizer program. Type help or ? to list commands'
+
+    def do_a(self, line):
+        print 'doing command /"a/"'
+    
+    def help_a(self):
+        print 'this is help string for \"a"'
+
+    def do_r(self, line):
+        print 'doing command /"r/"'
+
+    def do_c(self, line):
+        print 'doing command /"c/"'
+
+    def do_g(self, line):
+        print 'doing command /"g/"'
+
+    def precmd(self, line):
+        return line
+
+    def postcmd(self, stop, line):
+        #If line empty, exit program
+        if not line:
+            stop = True
+        return stop
+
+    def postloop(self):
+        #Cleanup and gracefully exit
+        sys.exit(0)
+
+    def default(self, line):
+        print 'Error: The command you entered was not found'
+        print line
+
+    def emptyline(self):
+        pass
+        
+def parse(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('command', choices=['a','c','r','g'])
+    parser.add_argument('street_name')
+    parser.add_argument('coords', nargs='+')
+    try:
+        args = parser.parse_args(shlex.split(line))
+    except SystemExit:
+        print 'FAILURE ABORT'
+        return False
+    check_coordinate_input(args.coords)
+    print args.coords
+    return args.command, args.street_name, args.coords
+
+def check_coordinate_input(coords):
+    if len(coords)==0:
+        print "Error: No coordinates were entered"
+        return False
+    for coord in coords:
+        if not(coord.startswith('(') and coord.endswith(')')):
+            print 'Error: coordinate \"%s\" does not start with \'(\' and end with \')\'' % coord
+
+if __name__ == '__main__':
+    #main()
+    program = Cameraprog()
+    program.prompt = '=-> ' 
+    program.use_rawinput = False
+    program.cmdloop()
